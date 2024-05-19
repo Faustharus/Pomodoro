9e74a5d1c98f422185955773ed594a5be3402d3c
Faustharus <chailloleau-damien@outlook.fr>
2024-05-19 09:06:17 +0200

    Initial Commit

---
+++ Pomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by Damien Chailloleau on 19/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Damien Chailloleau on 19/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
